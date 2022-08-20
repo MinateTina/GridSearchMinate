600946c77cb6b9d66d47b6c7095cb1c5ba389658
MinateTina <tinatung509@gmail.com>
2022-08-20 10:51:19 -0700

    Initial Commit

---
+++ GridSearchMinate/GridSearchMinateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchMinateApp.swift
+//  GridSearchMinate
+//
+//  Created by Minate on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchMinateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
